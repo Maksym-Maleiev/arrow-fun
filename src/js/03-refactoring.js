170cb40e1d83446dadf18e68de9163213b5960c6
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-06 09:31:38 +0200

    Update 03-refactoring.js

---
+++ src/js/03-refactoring.js
@@ -0,0 +1,16 @@
+/*
+ * Ланцюжки викликів - chaining
+ */
+
+const numbers = [1, 5, 2, 4, 3];
+
+/*
+ * Сортуємо тих хто онлайн за рангом
+ */
+
+// const players = [
+//   { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
+//   { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
+//   { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
+//   { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
+// ];
