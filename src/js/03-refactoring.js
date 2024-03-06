f429ede738eec624557a3e1c708af03ee25362d5
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-06 09:39:31 +0200

    Update 03-refactoring.js

--- src/js/03-refactoring.js
+++ src/js/03-refactoring.js
@@ -5,10 +5,21 @@
 const numbers = [1, 5, 2, 4, 3];
 
 const greaterThenTwo = numbers.filter(num => num > 2);
-console.log(greaterThenTwo);
+// console.log(greaterThenTwo);
 
 const multByTwo = greaterThenTwo.map(num => num * 3);
-console.log(multByTwo);
+// console.log(multByTwo);
+
+const sorted = multByTwo.sort((a, b) => a - b);
+// console.log(sorted);
+
+const res = numbers
+  .filter(num => num > 2)
+  .map(num => num * 3)
+  .sort((a, b) => a - b);
+
+console.log(res);
+
 /*
  * Сортуємо тих хто онлайн за рангом
  */
