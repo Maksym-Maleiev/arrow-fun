02465367bba4e47ffda2e42a4e17f6ee50044e7b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-06 09:45:00 +0200

    Update 03-refactoring.js

--- src/js/03-refactoring.js
+++ src/js/03-refactoring.js
@@ -18,26 +18,32 @@ const res = numbers
   .map(num => num * 3)
   .sort((a, b) => a - b);
 
-console.log(res);
+// console.log(res);
 
 /*
  * Сортуємо тих хто онлайн за рангом
  */
 
-// const players = [
-//   { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
-//   { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
-//   { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
-//   { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
-// ];
-
 const players = [
-  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
-  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+  { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
+  { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
+  { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
+  { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
 ];
+
+const onlineAndSorted = players
+  .filter(player => player.isOnline)
+  .sort((prevPlayer, nextPlayer) => prevPlayer.rank - nextPlayer.rank);
+
+console.table(onlineAndSorted);
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
 
 /*
  * Збільшуємо кількість поінтів кожного гравця
