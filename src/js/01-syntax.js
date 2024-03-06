f2f88665c0e9f9097863026bd424b4136edfeecb
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-06 08:53:40 +0200

    Update 01-syntax.js

--- src/js/01-syntax.js
+++ src/js/01-syntax.js
@@ -21,14 +21,23 @@
 
 // logMessage('Hello');
 
-const greet = () => {
-  console.log('Hello');
-};
+// const greet = () => {
+//   console.log('Hello');
+// };
 
-greet();
+// greet();
+
 /*
  * Явне і неявне повернення
  */
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+const add = (a, b) => a + b;
+
+console.log(add(2, 3));
 
 /*
  * Псевдомасив arguments
