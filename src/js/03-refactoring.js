e672f7384ff8b76473a12474a63009893fe9a452
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-06 09:32:34 +0200

    Update 03-refactoring.js

--- src/js/03-refactoring.js
+++ src/js/03-refactoring.js
@@ -14,3 +14,19 @@ const numbers = [1, 5, 2, 4, 3];
 //   { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
 //   { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
 // ];
+
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
+
+/*
+ * Збільшуємо кількість поінтів кожного гравця
+ */
+
+/*
+ * Збільшуємо кількість годин гравця по id
+ */
